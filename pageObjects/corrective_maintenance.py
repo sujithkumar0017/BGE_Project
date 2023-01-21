080ec1511ba31a77d5886ed79fc916d28a79399f
sujithkumar0017 <sujith.tesark@gmail.com>
2023-01-21 16:52:36 +0530

    updated changes

--- pageObjects/corrective_maintenance.py
+++ pageObjects/corrective_maintenance.py
@@ -1,3 +1,4 @@
+import os
 import time
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
@@ -74,7 +75,7 @@ class corrective_maintenance:
         actions.click(element)
         actions.send_keys(field_engineer)
         actions.send_keys(Keys.ENTER).perform()
-    def startdate(self):
+    def start_date(self):
         pass
     def assigned_to(self,assigned_to):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,self.assigned_to_xpath)))
@@ -93,16 +94,17 @@ class corrective_maintenance:
     def labour_hours(self,labour_hours):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,self.labour_hours_xpath)))
         element.send_keys(labour_hours)
-    def description(self,descripition):
+    def description(self,description):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,self.description_xpath)))
-        element.send_keys(descripition)
+        element.send_keys(description)
     def comment(self,comment):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,self.comment_xpath)))
         element.send_keys(comment)
     def parent_task(self):
         pass
     def attachments(self):
-        pass
+        file_to_upload_path = os.getcwd() + "/Files/file.png"
+        self.driver.find_element(By.XPATH,'//input[@type="file"]').send_keys(file_to_upload_path)
     def add_corrective(self):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,self.add_button_xpath)))    
         element.click()
@@ -113,4 +115,88 @@ class corrective_maintenance:
         assert "Plant name is required"== self.driver.find_element(By.XPATH,'//span[normalize-space()="Plant name is required"]').text
         assert "Field Engineer is required"== self.driver.find_element(By.XPATH,'//span[normalize-space()="Field Engineer is required"]').text
         assert "Assigned To is required"== self.driver.find_element(By.XPATH,'//span[normalize-space()="Assigned To is required"]').text
-    def 
+
+
+
+
+    #Filter Functionality
+    def filter_option(self):
+        self.driver.find_element(By.XPATH,'//div[@tag="a"]').click()
+    def apply_button(self):
+        self.driver.find_element(By.CSS_SELECTOR,".btn.btn-secondary").click()  
+    def status(self):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,'(//div[@class="react-select__input"]//input[@type="text"])[1]')))
+        actions = ActionChains(self.driver)
+        actions.click(element)
+        actions.send_keys("open")
+        actions.send_keys(Keys.ENTER).perform()
+        self.apply_button()
+        
+        # sleep = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.ID,"client-next-btn")))
+        isNextDisabled = False
+        while not isNextDisabled:
+             element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+             (By.XPATH, '//div[@class="user-name"]/following::span[@class="text-capitalize badge badge-danger badge-pill"]')))
+             table = element.find_element(By.XPATH, '//div[@class="user-name"]/following::span[@class="text-capitalize badge badge-danger badge-pill"]')
+             validate_status = table.find_elements(By.XPATH,'//div[@class="user-name"]/following::span[@class="text-capitalize badge badge-danger badge-pill"]')
+             for x in validate_status:
+                print(x.text)
+             nxt_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//div[@class='card-inner']//li[5]")))
+             next_class = nxt_btn.get_attribute('class')  
+             if "page-item disabled" in next_class:
+                    isNextDisabled = True
+             else:
+                self.driver.find_element(By.XPATH,"//div[@class='card-inner']//li[5]").click()
+    def assigned_engineer(self):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,'(//div[@class="react-select__input"]//input[@type="text"])[2]')))
+        actions = ActionChains(self.driver)
+        actions.click(element)
+        actions.send_keys("Admin Test")
+        actions.send_keys(Keys.ENTER).perform()
+        self.apply_button()
+        isNextDisabled = False
+        while not isNextDisabled:
+             element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+             (By.XPATH, '//div[@class="user-name"]/following::span[@class="text-capitalize badge badge-danger badge-pill"]')))
+             table = element.find_element(By.XPATH, '//div[@class="user-name"]/following::span[@class="text-capitalize badge badge-danger badge-pill"]')
+             validate_status = table.find_elements(By.XPATH,'//div[@class="user-name"]/following::span[@class="text-capitalize badge badge-danger badge-pill"]')
+             for x in validate_status:
+                print(x.text)
+             nxt_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,"//div[@class='card-inner']//li[5]")))
+             next_class = nxt_btn.get_attribute('class')  
+             if "page-item disabled" in next_class:
+                    isNextDisabled = True
+             else:
+                self.driver.find_element(By.XPATH,"//div[@class='card-inner']//li[5]").click()
+    def plant_name(self):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,'(//div[@class="react-select__input"]//input[@type="text"])[3]')))
+        actions = ActionChains(self.driver)
+        actions.click(element)
+        actions.send_keys("coffee plant")
+        actions.send_keys(Keys.ENTER).perform()
+        self.apply_button()
+
+    def start_date(self):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,'(//div[@class="react-datepicker-wrapper"])[1]')))
+        actions = ActionChains(self.driver)
+        actions.click(element)
+        actions.send_keys("09/01/2023")
+        actions.send_keys(Keys.ENTER).perform()
+        self.apply_button()
+    def resolved_date(self):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,'(//div[@class="react-datepicker-wrapper"])[2]')))
+        actions = ActionChains(self.driver)
+        actions.click(element)
+        actions.send_keys("09/01/2023")
+        actions.send_keys(Keys.ENTER).perform()
+        self.apply_button()
+
+
+
+
+
+
+"""
+    ticket name = //div[@class="user-name"]
+    status = //div[@class="user-name"]/following::span[@class="text-capitalize badge badge-danger badge-pill"]
+"""
